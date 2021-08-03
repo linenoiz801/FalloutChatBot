8e537f73a7fd6a3be121dd19aff6c53ca69a9702
Paul Gailey <linenoiz@gmail.com>
2021-08-03 10:42:14 -0600

    Azure deployment success!

---
+++ FalloutChat.Data/Migrations/202108031637150_InitialMigrain.cs
@@ -0,0 +1,16 @@
+﻿namespace FalloutChat.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialMigrain : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
---
+++ FalloutChat.Data/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace FalloutChat.Data.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<FalloutChat.Data.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "FalloutChat.Data.ApplicationDbContext";
+        }
+
+        protected override void Seed(FalloutChat.Data.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
